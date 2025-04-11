29b109f85f99879bee3c96cc24a095a90bd59196
Damien Julia <damien.julia@essec.edu>
2025-04-11 15:50:27 -0700

    small quality update

--- pages/api/generate_score.js
+++ pages/api/generate_score.js
@@ -30,19 +30,19 @@ Your role is to answer the list of questions below. ONLY ACCEPTABLE ANSWERS ARE 
 
 General assessment
 1) Would you consider this petition to be a model petition, that would be in the top 5% of petitions? Yes | No
-2) Would you suggest Change.org could promote this petition via email, social media, or on our homepage? Yes | No 
-3) Does this petition include the specific details and clear calls to action needed for it to be seen as credible by journalists or public officials? Yes | No 
-4) Would you categorize it a low-quality petition (non-serious, joke, unrealistic expectations, offensive, etc.)? Yes | No
-5) How realistic is it for this petition to win = achieve the desired impact? Realistic | Aspirational | Unlikely 
+2) Would you recommend that Change.org promote this petition through email, social media, or the homepage Yes | No 
+3) Does this petition include the specific details and clear calls to action needed for it to be seen as credible by journalists or decision-makers? Yes | No 
+4) Is this petition likely to influence actual policy, based on how it frames the issue and the clarity of its demands? Yes | No
+5) Would you categorize it a low-quality petition (non-serious, joke, unrealistic expectations, offensive, etc.)? Yes | No
 
 
 Concrete opportunities for improvement
-5) Does the petition call for a specific change and is that goal clear in the title? Yes | No 
-6) Does the petition description offer concrete and actionable solutions to the issue? Yes | No
-7) Does the petition mention a clear target or decision-maker? Yes | No
-8) Does the petition feature a personal story that would help supporters empathize with the issue? Yes | No
-9) Does the petition feature evidence and supporting data? Yes | No
-10) Is the petition clearly organized, has proper grammar and no spelling errors? Yes | No 
+6) Does the petition call for a specific change and is that goal clear in the title? Yes | No 
+7) Does the petition description offer concrete and actionable solutions to the issue? Yes | No
+8) Does the petition mention a clear target or decision-maker? Yes | No
+9) Does the petition feature a personal story that would help supporters empathize with the issue? Yes | No
+10) Does the petition feature evidence and supporting data? Yes | No
+11) Is the petition clearly organized, has proper grammar and no spelling errors? Yes | No 
 
 
 Your reply ONLY features this list of questions in order, with the response after each question. NOTHING MORE. `,
